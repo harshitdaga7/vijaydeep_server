1e46b7a9c0c96fa14bcd5152b820000ce19454ea
Harshit Daga <78150691+harshitdaga7@users.noreply.github.com>
2021-12-21 19:06:05 +0530

    working home page

--- app.js
+++ app.js
@@ -19,8 +19,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+/**
+ * Routes :
+ * "/"
+ * "/about"
+ * "/products"
+ * "/news"
+ * "/contact"
+ * "/team"
+ * "/auth"
+ * "/console"
+ */
+
+
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.status(404);
+  res.render('index', { title: 'hell1' });
 });
 
 module.exports = router;
